852c4d081179752f963edb7100d1b223a9c59ee6
Ed Ceaser <eac@twitter.com>
2010-02-12 15:22:37 -0800

    fix hashmap synchronization

--- src/main/scala/com/twitter/querulous/database/MemoizingDatabase.scala
+++ src/main/scala/com/twitter/querulous/database/MemoizingDatabase.scala
@@ -3,7 +3,7 @@ package com.twitter.querulous.database
 import scala.collection.mutable
 
 class MemoizingDatabaseFactory(databaseFactory: DatabaseFactory) extends DatabaseFactory {
-  private val databases = new mutable.HashMap[String, Database]
+  private val databases = new mutable.HashMap[String, Database] with mutable.SynchronizedMap[String, Database]
 
   def apply(dbhosts: List[String], dbname: String, username: String, password: String) = synchronized {
     databases.getOrElseUpdate(

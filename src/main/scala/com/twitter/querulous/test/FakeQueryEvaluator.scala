5df51582f4048fda47c1e75a71fb8579d4fb5849
Kyle Maxwell <kmaxwell@twitter.com>
2010-09-14 12:50:30 -0700

    batch queries/prepared statements

--- src/main/scala/com/twitter/querulous/test/FakeQueryEvaluator.scala
+++ src/main/scala/com/twitter/querulous/test/FakeQueryEvaluator.scala
@@ -3,12 +3,14 @@ package com.twitter.querulous.test
 import java.sql.ResultSet
 import com.twitter.xrayspecs.Duration
 import com.twitter.querulous.evaluator.{Transaction, QueryEvaluator}
+import com.twitter.querulous.query.Query
 
 class FakeQueryEvaluator[A](trans: Transaction, resultSets: Seq[ResultSet]) extends QueryEvaluator {
   def select[A](query: String, params: Any*)(f: ResultSet => A) = resultSets.map(f)
   def selectOne[A](query: String, params: Any*)(f: ResultSet => A) = None
   def count(query: String, params: Any*) = 0
   def execute(query: String, params: Any*) = 0
+  def executeBatch(query: String)(f: Query => Unit) = 0
   def nextId(tableName: String) = 0
   def insert(query: String, params: Any*) = 0
   def transaction[T](f: Transaction => T) = f(trans)

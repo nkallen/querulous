5ec8762d65036adc6f87ccf1f664be18c3f39fee
Kyle Maxwell <kmaxwell@twitter.com>
2010-09-22 18:52:39 -0700

    more efficient sql

--- src/main/scala/com/twitter/querulous/database/Database.scala
+++ src/main/scala/com/twitter/querulous/database/Database.scala
@@ -60,7 +60,7 @@ trait Database {
   protected def url(dbhosts: List[String], dbname: String, urlOptions: Map[String, String]) = {
     val dbnameSegment = if (dbname == null) "" else ("/" + dbname)
     val urlOptsSegment = if (urlOptions == null) {
-      "?useUnicode=true&characterEncoding=UTF-8"
+      "?useUnicode=true&characterEncoding=UTF-8&rewriteBatchedStatements=true"
     } else {
       "?" + urlOptions.keys.map( k => k + "=" + urlOptions(k) ).mkString("&")
     }

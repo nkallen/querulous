5c8cb2f84d2e555426a21cd3d4fa199a598c8ae7
Matt Freels <matt@freels.name>
2010-11-11 23:42:05 -0800

    pass stats collector down through QueryEvaluatorFactory config

--- src/main/scala/com/twitter/querulous/config/QueryEvaluator.scala
+++ src/main/scala/com/twitter/querulous/config/QueryEvaluator.scala
@@ -3,7 +3,6 @@ package com.twitter.querulous.config
 import com.twitter.util.Duration
 import evaluator._
 
-
 trait AutoDisablingQueryEvaluator {
   def errorCount: Int
   def interval: Duration
@@ -15,13 +14,17 @@ trait QueryEvaluator {
 
   def autoDisable: Option[AutoDisablingQueryEvaluator]
 
-  def apply() = {
-    var factory: QueryEvaluatorFactory = new StandardQueryEvaluatorFactory(database(), query())
-    autoDisable.foreach { disableConfig =>
-      factory = new AutoDisablingQueryEvaluatorFactory(factory,
-                                                       disableConfig.errorCount,
-                                                       disableConfig.interval)
+  def apply(stats: StatsCollector) = {
+    var factory: QueryEvaluatorFactory =
+      new StandardQueryEvaluatorFactory(database(), query(stats))
+
+    autoDisable.foreach { disable =>
+      factory = new AutoDisablingQueryEvaluatorFactory(
+        factory, disable.errorCount, disable.interval
+      )
     }
     factory
   }
+
+  def apply(): QueryEvaluatorFactory = apply(NullStatsCollector)
 }

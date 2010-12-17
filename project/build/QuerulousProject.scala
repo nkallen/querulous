853e6e589663c9802be0ebaeadca1c773722a275
Matt Freels <matt@freels.name>
2010-12-16 16:24:46 -0800

    fix cross-compile util dependency

--- project/build/QuerulousProject.scala
+++ project/build/QuerulousProject.scala
@@ -8,14 +8,20 @@ class QuerulousProject(info: ProjectInfo) extends StandardProject(info) with Sub
     case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
     case _ => "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
   }
+
   buildScalaVersion match {
     case "2.7.7" => inline("net.lag" % "configgy" % "1.6.6")
     case _ => inline("net.lag" % "configgy" % "2.0.0")
   }
+
+  val util =   buildScalaVersion match {
+    case "2.7.7" => "com.twitter" % "util" % "1.1.1"
+    case _       => "com.twitter" % "util" % "1.2.8"
+  }
+
   val dbcp      = "commons-dbcp" % "commons-dbcp" % "1.4"
   val mysqljdbc = "mysql" % "mysql-connector-java" % "5.1.13"
   val pool      = "commons-pool" % "commons-pool" % "1.5.4"
-  val util      = "com.twitter" % "util" % "1.1.1"
   val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
   val jmock     = "org.jmock" % "jmock" % "2.4.0" % "test"
   val cglib     = "cglib" % "cglib" % "2.1_3" % "test"

6ab31727305a4d8cd486f29e70141572d33e9801
Jeremy Cloud <jeremycloud@twitter.com>
2011-07-05 16:15:56 -0400

    util 1.10.1

--- project/build/QuerulousProject.scala
+++ project/build/QuerulousProject.scala
@@ -6,12 +6,12 @@ class QuerulousProject(info: ProjectInfo) extends StandardLibraryProject(info)
 with DefaultRepos
 with SubversionPublisher {
 
-  val utilCore  = "com.twitter"  % "util-core"            % "1.8.13"
+  val utilCore  = "com.twitter"  % "util-core"            % "1.10.1"
   val dbcp      = "commons-dbcp" % "commons-dbcp"         % "1.4"
   val mysqljdbc = "mysql"        % "mysql-connector-java" % "5.1.13"
   val pool      = "commons-pool" % "commons-pool"         % "1.5.4"
 
-  val utilEval   = "com.twitter"             % "util-eval"      % "1.8.13" % "test"
+  val utilEval   = "com.twitter"             % "util-eval"      % "1.10.1" % "test"
   val scalaTools = "org.scala-lang"          % "scala-compiler" % "2.8.1"  % "test"
   val hamcrest   = "org.hamcrest"            % "hamcrest-all"   % "1.1"    % "test"
   val specs      = "org.scala-tools.testing" % "specs_2.8.0"    % "1.6.5"  % "test"

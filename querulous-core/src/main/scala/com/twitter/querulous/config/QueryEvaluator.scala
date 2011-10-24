28c1da9d110d023938087a84ee647de78be883cb
Matt Freels <matt@freels.name>
2011-10-24 11:44:27 -0700

    flag on config to allow singleton factories per config object

--- querulous-core/src/main/scala/com/twitter/querulous/config/QueryEvaluator.scala
+++ querulous-core/src/main/scala/com/twitter/querulous/config/QueryEvaluator.scala
@@ -14,22 +14,34 @@ trait AutoDisablingQueryEvaluator {
 class QueryEvaluator {
   var database: Database = new Database
   var query: Query = new Query
+  var singletonFactory = false
 
   var autoDisable: Option[AutoDisablingQueryEvaluator] = None
   def autoDisable_=(a: AutoDisablingQueryEvaluator) { autoDisable = Some(a) }
 
+  private var memoizedFactory: Option[QueryEvaluatorFactory] = None
+
   def apply(stats: StatsCollector): QueryEvaluatorFactory = apply(stats, None, None)
   def apply(stats: StatsCollector, dbStatsFactory: DatabaseFactory => DatabaseFactory, queryStatsFactory: QueryFactory => QueryFactory): QueryEvaluatorFactory = apply(stats, Some(dbStatsFactory), Some(queryStatsFactory))
-  def apply(stats: StatsCollector, dbStatsFactory: Option[DatabaseFactory => DatabaseFactory], queryStatsFactory: Option[QueryFactory => QueryFactory]): QueryEvaluatorFactory = {
-    var factory: QueryEvaluatorFactory =
-      new StandardQueryEvaluatorFactory(database(stats, dbStatsFactory), query(stats, queryStatsFactory))
 
-    autoDisable.foreach { disable =>
-      factory = new AutoDisablingQueryEvaluatorFactory(
-        factory, disable.errorCount, disable.interval
-      )
+  def apply(stats: StatsCollector, dbStatsFactory: Option[DatabaseFactory => DatabaseFactory], queryStatsFactory: Option[QueryFactory => QueryFactory]): QueryEvaluatorFactory = {
+    synchronized {
+      if (!singletonFactory) memoizedFactory = None
+
+      memoizedFactory = memoizedFactory orElse {
+        var factory: QueryEvaluatorFactory = new StandardQueryEvaluatorFactory(database(stats, dbStatsFactory), query(stats, queryStatsFactory))
+
+        autoDisable.foreach { disable =>
+          factory = new AutoDisablingQueryEvaluatorFactory(
+            factory, disable.errorCount, disable.interval
+          )
+        }
+
+        Some(factory)
+      }
+
+      memoizedFactory.get
     }
-    factory
   }
 
   def apply(): QueryEvaluatorFactory = apply(NullStatsCollector)

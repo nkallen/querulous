28e4957d0da4b8a6f1fdc3725d1ec02e73688101
Jeremy Cloud <jeremycloud@twitter.com>
2012-01-25 07:38:00 -0800

    latest finagle and util

--- project/build/QuerulousProject.scala
+++ project/build/QuerulousProject.scala
@@ -28,8 +28,8 @@ with SubversionPublisher {
     with DefaultRepos
     with SubversionPublisher
 
-  val utilVersion    = "1.12.7"
-  val finagleVersion = "1.9.10"
+  val utilVersion    = "1.12.12"
+  val finagleVersion = "1.10.0"
 
   class CoreProject(info: ProjectInfo) extends StandardLibraryProject(info) with Defaults {
     val utilCore  = "com.twitter"  % "util-core"            % utilVersion
